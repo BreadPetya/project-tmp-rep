95da7bdf2f53f42b6ef34637c645d73f0427999b
Глазов Николай <n.glazov@infinet.ru>
2025-07-15 11:17:17 +0500

    Up

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import setuptools
+#import setuptools
 
-setuptools.setup(name="project-tmp-rep")
+#setuptools.setup(name="project-tmp-rep")
 
-from project_tmp_rep.project import func_from_project
+# from project_tmp_rep.project import func_from_project
 
-if __name__ == "__main__":
-    print(func_from_project())
+# if __name__ == "__main__":
+#     print(func_from_project())
+from lib_tmp_rep.func_lib import func_from_lib
+
+def func_from_project():
+    return func_from_lib() + 1
